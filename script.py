12de8ff9fc75bbb52cb92b67585f73f5f097aad5
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-08-02 12:22:01 -0300

    improved code and added conditionals

--- script.py
+++ script.py
@@ -3,9 +3,28 @@ from mysql.connector import connect, Error
 
 try:
 	conn = connect(
-			host='localhost',
-			user=input('Username: '),
-			password=getpass('Password: ')
-		)
+		host='localhost',
+		user=input('Username: '),
+		password=getpass('Password: ')
+	)
+	
+	cursor = conn.cursor() # para interagir com o BD
+    
+	cursor.execute("SHOW DATABASES LIKE 'online_movie_rating';")
+	database_exists = cursor.fetchone()
+
+	if database_exists:
+		print('Database exists already!')
+	else:
+		create_db_query = 'CREATE DATABASE online_movie_rating'
+		cursor.execute(create_db_query)
+		print('Database created successfully!')
+
+	show_db_query = 'SHOW DATABASES;'
+
+	cursor.execute(show_db_query)
+	for db in cursor: # mostra todos os BD
+		print(db)
+
 except Error as e:
 	print(e)
